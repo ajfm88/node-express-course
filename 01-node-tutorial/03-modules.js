b9774f7482b12a0bdcf45f37499c93ae8da0cc81
unknown <ajfoucaultmo@mail.usf.edu>
2022-07-05 21:27:46 -0700

    04. First Module

--- 01-node-tutorial/03-modules.js
+++ 01-node-tutorial/03-modules.js
@@ -1,11 +1,8 @@
-// Modules
-const john = 'john';
-const peter = 'peter';
-
-const sayHi = (name) => {
-  console.log(`Hello there ${name}`);
-};
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+const names = require('./04-names');
+const sayHi = require('./05-utils');
 
 sayHi('susan');
-sayHi(john);
-sayHi(peter);
+sayHi(names.john);
+sayHi(names.peter);

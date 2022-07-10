f9fa160bbba1230810d3f3c2f9e9eb7a12092c5c
unknown <ajfoucaultmo@mail.usf.edu>
2022-07-10 09:37:32 -0700

    44. Events Emitter - Additional Info

--- 01-node-tutorial/13-event-emitter.js
+++ 01-node-tutorial/13-event-emitter.js
@@ -2,8 +2,10 @@ const EventEmitter = require('events');
 
 const customEmitter = new EventEmitter();
 
-customEmitter.on('response', () => {
-  console.log(`data recieved`);
+customEmitter.on('response', (name, id) => {
+  console.log(`data recieved user ${name} with id:${id}`);
 });
-
-customEmitter.emit('response');
+customEmitter.on('response', () => {
+  console.log('some other logic here');
+});
+customEmitter.emit('response', 'john', 34);

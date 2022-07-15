c36b92fcbad985b7bf7ffb1a94735468ab7d9b4e
unknown <ajfoucaultmo@mail.usf.edu>
2022-07-14 20:59:09 -0700

    68. Params

--- 02-express-tutorial/final/07-params-query.js
+++ 02-express-tutorial/final/07-params-query.js
@@ -15,6 +15,21 @@ app.get('/api/products', (req, res) => {
   res.json(newProducts);
 });
 
+app.get('/api/products/:productID', (req, res) => {
+  // console.log(req)
+  // console.log(req.params)
+  const { productID } = req.params;
+
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send('Product Does Not Exist');
+  }
+
+  return res.json(singleProduct);
+});
+
 app.listen(5000, () => {
   console.log('Server is listening on port 5000....');
 });
